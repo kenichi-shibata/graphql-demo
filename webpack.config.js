f993cb66c5474eba2882ab67718b17fe43180351
Kenichi Shibata <kenichi-shibata@outlook.com>
2018-03-19 15:49:00 +0000

    Move the npm run dev (#1)
    * merge upstream
    
    * using webpack
    
    * Fixed issues with html-webpack-plugin
    
    * Node server don't need html and dev-server
    
    * Change the npm run dev

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: 'node', // don't touch any built in node native modules (fs et.al.)
+  externals: [nodeExternals()], // don\t touch node_modules
+
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+
+};
+
